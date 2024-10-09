2c0d1190a9818ded9e721bb8048a37cbb5e5c4ad
KaliaGonzalez <k.gonzalezj@uniandes.edu.co>
2024-10-09 15:02:17 -0500

    cambio estupido

--- monitoring/urls.py
+++ monitoring/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('', views.index),
     path('', include('measurements.urls')),
     path('', include('variables.urls')),
-    path('health-check/', views.healtCheck),
+    path('health-check/', views.healthCheck),
 ]
