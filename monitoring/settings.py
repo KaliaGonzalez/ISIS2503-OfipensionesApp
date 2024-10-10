6ee57218843580113f3d2d85a51c397408852bf1
KaliaGonzalez <k.gonzalezj@uniandes.edu.co>
2024-10-10 06:59:28 -0500

    Cambioo yaaaa yaa ayaa

--- monitoring/settings.py
+++ monitoring/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'logs_ofipensiones',
-
+    'logs_ofipensiones', 
 ]
 
 MIDDLEWARE = [
