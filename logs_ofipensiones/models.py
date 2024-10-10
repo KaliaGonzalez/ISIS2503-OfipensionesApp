f6307ebe6cf65f85c756b4c9e22a9e8660a1e8d9
KaliaGonzalez <k.gonzalezj@uniandes.edu.co>
2024-10-10 06:54:39 -0500

    cambio bobobooooo

--- logs_ofipensiones/models.py
+++ logs_ofipensiones/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Logs(models.Model):
-    nombre = models.CharField(max_lenght=40)
+    nombre = models.CharField(max_length=40)
     descripcion =  models.CharField(max_length=50)
     accion = models.CharField(max_length=50)
 
