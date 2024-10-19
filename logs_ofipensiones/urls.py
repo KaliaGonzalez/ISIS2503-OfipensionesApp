43141aa5c379cd74ee04c39f8c898edd27ff68b4
KaliaGonzalez <k.gonzalezj@uniandes.edu.co>
2024-10-19 01:13:43 -0500

    aaaaaaaaaaaaaa

--- logs_ofipensiones/urls.py
+++ logs_ofipensiones/urls.py
@@ -4,6 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from . import views
 
 urlpatterns = [
-    path('logs_ofipensiones/', views.logs_list),
+    path('logs_ofipensiones/', views.logs_list,name='logs_list'),
     path('logcreate/', csrf_exempt(views.log_create), name='logCreate'),
 ]
